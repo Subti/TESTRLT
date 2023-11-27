77e6679d9aeeff46bfa9814c9aa5a489d04a59b9
JP Saliba <jpsaliba@outlook.com>
2023-11-27 15:03:19 -0500

    Test push.

---
+++ main.js
@@ -0,0 +1,94 @@
+import "./style.css";
+import Phaser from "phaser";
+
+const sizes = {
+  width: 600,
+  height: 600,
+};
+
+const speedDown = 150;
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("scene-game");
+    this.player;
+  }
+
+  preload() {
+    this.load.image("bg", "assets/bg.jpg");
+    this.load.spritesheet("player", "assets/player.png", {
+      frameWidth: 62,
+      frameHeight: 64,
+      startFrame: 5,
+    });
+    this.load.spritesheet("invisibleSprite", "assets/invisibleSprite.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+      startFrame: 0,
+      endFrame: 0,
+    });
+  }
+  create() {
+    this.add.image(0, 0, "bg");
+
+    this.targetWord = "example";
+    this.currentWord = "";
+
+    this.words = this.physics.add.group();
+
+    this.targetWordSprite = this.words
+      .create(10, 10, "invisibleSprite")
+      .setScale(0.5);
+
+    this.targetWordText = this.add.text(10, 10, this.targetWord, {
+      fontSize: "32px",
+      fill: "#fff",
+    });
+
+    this.currentWordText = this.add.text(10, 50, this.currentWord, {
+      fontSize: "32px",
+      fill: "#fff",
+    });
+
+    this.input.keyboard.on("keydown", (event) => {
+      this.currentWord += event.key;
+
+      this.currentWordText.setText(this.currentWord);
+
+      if (this.currentWord === this.targetWord) {
+        this.currentWord = "";
+        this.currentWordText.setText(this.currentWord);
+
+        this.targetWordText.destroy();
+        this.targetWordSprite.destroy();
+      }
+    });
+
+    this.player = this.add
+      .sprite(sizes.width / 2, sizes.height - 100, "player")
+      .setOrigin(0, 0);
+
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+  }
+  update() {
+    this.targetWordText.x = this.targetWordSprite.x;
+    this.targetWordText.y = this.targetWordSprite.y;
+  }
+}
+
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: speedDown },
+      debug: true,
+    },
+  },
+  scene: [GameScene],
+};
+
+const game = new Phaser.Game(config);
