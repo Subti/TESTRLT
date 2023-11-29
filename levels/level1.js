a92d782fb42b0990377776a8be0acfc0797f2ff2
JP Saliba <jpsaliba@outlook.com>
2023-11-29 16:35:23 -0500

    Globalize variables, implement win loss.

--- levels/level1.js
+++ levels/level1.js
@@ -1,21 +1,11 @@
-import "./style.css";
-import Phaser from "phaser";
-
-//Size of game world
-const sizes = {
-  width: 1200,
-  height: 600,
-};
-
-let speedDown = 150;
-
 //define game canvas (what it does)
-class GameScene extends Phaser.Scene {
+export class level1 extends Phaser.Scene {
   constructor() {
     super("scene-game");
     this.player;
     this.activeWords = [];
     this.calledWords = [];
+    this.points = 0;
   }
 
   //preload assets, anything that needs to be loaded before the game starts (images, sprites, etc)
@@ -33,14 +23,15 @@ class GameScene extends Phaser.Scene {
       startFrame: 0,
       endFrame: 0,
     });
+  }
 
+  //create assets, anything that needs to be added/loaded to the game world (images, sprites, etc), as well as initial game logic and physics
+  create() {
     //batch call the api to get a bunch of words at once
     fetch("https://random-word-api.vercel.app/api?words=5&length=5")
       .then((response) => response.json())
       .then((data) => {
         this.calledWords = data;
-
-        this.registry.set("loaded", true);
 
         //Load a word at random intervals of 1-3 seconds
         this.time.addEvent({
@@ -51,14 +42,6 @@ class GameScene extends Phaser.Scene {
           repeat: this.calledWords.length - 1,
         });
       });
-  }
-
-  //create assets, anything that needs to be added/loaded to the game world (images, sprites, etc), as well as initial game logic and physics
-  create() {
-    this.registry.set("points", 0);
-    this.registry.set("speedDown", 150);
-    this.registry.set("isColliding", false);
-    this.registry.set("lives", 3);
 
     //this initializes what the player is currently typing
     this.currentWord = "";
@@ -103,8 +86,8 @@ class GameScene extends Phaser.Scene {
     this.platform = this.physics.add.staticGroup();
     this.platform.create(600, 600, "platform").setScale(3).refreshBody();
     this.words.addCollidesWith(this.platform);
-    this.physics.add.overlap(this.words, this.platform, () => {
-      this.registry.set("isColliding", true);
+    this.physics.add.overlap(this.words, this.platform, function () {
+      isColliding = true;
     });
 
     this.player = this.add
@@ -125,7 +108,7 @@ class GameScene extends Phaser.Scene {
     const sprite = this.words
       .create(Phaser.Math.Between(0, 1025), 10, "invisibleSprite")
       .setScale(0.5)
-      .setVelocityY(this.registry.get("speedDown") - 90);
+      .setVelocityY(speedDown - 90);
     sprite.body.setAllowGravity(false);
 
     const text = this.add.text(10, 10, word, {
@@ -138,10 +121,6 @@ class GameScene extends Phaser.Scene {
 
   //update assets, anything that needs to be updated every frame (images, sprites, etc), as well as game logic and physics
   update() {
-    if (!this.registry.get("loaded")) {
-      return;
-    }
-
     for (let i = 0; i < this.activeWords.length; i++) {
       this.activeWords[i].text.x = this.activeWords[i].sprite.x;
       this.activeWords[i].text.y = this.activeWords[i].sprite.y - 15;
@@ -150,11 +129,8 @@ class GameScene extends Phaser.Scene {
     // Check if the current word matches any active word
     for (let i = 0; i < this.activeWords.length; i++) {
       if (this.currentWord === this.activeWords[i].word) {
-        this.registry.set(
-          "points",
-          this.registry.get("points") + 10 * this.currentWord.length
-        );
-        this.textScore.setText(`Score: ${this.registry.get("points")}`);
+        this.points += 10 * this.currentWord.length;
+        this.textScore.setText(`Score: ${this.points}`);
         // Remove the word from the screen and the array
         this.activeWords[i].sprite.destroy();
         this.activeWords[i].text.destroy();
@@ -166,67 +142,12 @@ class GameScene extends Phaser.Scene {
 
         break;
       }
-      if (this.registry.get("isColliding")) {
+      if (isColliding) {
         this.activeWords[i].sprite.destroy();
         this.activeWords[i].text.destroy();
         this.activeWords.splice(i, 1);
-        this.registry.set("isColliding", false);
-        this.registry.set("lives", this.registry.get("lives") - 1);
+        isColliding = false;
       }
     }
-    // Check for loss condition
-    if (this.registry.get("lives") <= 0) {
-      // Transition to loss scene
-      this.scene.start("LossScene");
-    }
-
-    // Check for win condition
-    if (
-      this.calledWords.length === 0 &&
-      this.activeWords.length === 0 &&
-      this.registry.get("lives") > 0
-    ) {
-      // Transition to win scene
-      this.scene.start("WinScene");
-    }
   }
 }
-
-class WinScene extends Phaser.Scene {
-  constructor() {
-    super({ key: "WinScene" });
-  }
-
-  create() {
-    this.add.text(100, 100, "You win!", { fontSize: "32px", fill: "#fff" });
-  }
-}
-
-class LossScene extends Phaser.Scene {
-  constructor() {
-    super({ key: "LossScene" });
-  }
-
-  create() {
-    this.add.text(100, 100, "You lose!", { fontSize: "32px", fill: "#fff" });
-  }
-}
-
-//define game config (how it looks and works this is what players would change in settings)
-//for example if a player wants the game to be "easier" it could just be an option to use a lower gravity
-const config = {
-  type: Phaser.WEBGL,
-  width: sizes.width,
-  height: sizes.height,
-  canvas: gameCanvas,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: speedDown },
-      debug: true,
-    },
-  },
-  scene: [GameScene, WinScene, LossScene],
-};
-
-const game = new Phaser.Game(config);
