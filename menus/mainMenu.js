268984a85a293f3b16d6e264f73da3593a9f9b04
Kennedy Gosnell <kennicodes@outlook.com>
2023-11-30 14:35:17 -0800

    add fading animation in options menu for the 'backButton', edit main menu with example background animation

--- menus/mainMenu.js
+++ menus/mainMenu.js
@@ -4,12 +4,12 @@ export class MenuScene extends Phaser.Scene {
   }
   preload() {
     // From phaser example
-    // this.load.image('raster', 'assets/raster.png');
+    this.load.image('raster', 'assets/raster.png');
     // Also from phaser example
-    // this.load.audioSprite('sfx', 'assets/audio/SoundEffects/fx_mixdown.json', [
-    //   'assets/audio/SoundEffects/fx_mixdown.ogg',
-    //   'assets/audio/SoundEffects/fx_mixdown.mp3'
-    // ]);
+    this.load.audioSprite('sfx', 'assets/audio/SoundEffects/fx_mixdown.json', [
+      'assets/audio/SoundEffects/fx_mixdown.ogg',
+      'assets/audio/SoundEffects/fx_mixdown.mp3'
+    ]);
   }
 
   create() {
@@ -18,36 +18,36 @@ export class MenuScene extends Phaser.Scene {
     this.registry.set("isColliding", false);
     this.registry.set("lives", 3);
     // Test background from phaser example
-    // const group = this.add.group();
+    const group = this.add.group();
 
-    // group.createMultiple({ key: 'raster', repeat: 8 });
+    group.createMultiple({ key: 'raster', repeat: 8 });
 
-    // let ci = 0;
-    // const colors = [0xef658c, 0xff9a52, 0xffdf00, 0x31ef8c, 0x21dfff, 0x31aade, 0x5275de, 0x9c55ad, 0xbd208c];
+    let ci = 0;
+    const colors = [0xef658c, 0xff9a52, 0xffdf00, 0x31ef8c, 0x21dfff, 0x31aade, 0x5275de, 0x9c55ad, 0xbd208c];
 
-    // const _this = this;
+    const _this = this;
 
-    // group.children.iterate(child => {
+    group.children.iterate(child => {
 
-    //   child.x = -100;
-    //   child.y = 300;
-    //   child.depth = 9 - ci;
+      child.x = -100;
+      child.y = 300;
+      child.depth = 9 - ci;
 
-    //   child.tint = colors[ci];
+      child.tint = colors[ci];
 
-    //   ci++;
+      ci++;
 
-    //   _this.tweens.add({
-    //     targets: child,
-    //     x: 1300,
-    //     yoyo: true,
-    //     repeat: -1,
-    //     ease: 'Sine.easeInOut',
-    //     duration: 1500,
-    //     delay: 100 * ci
-    //   });
+      _this.tweens.add({
+        targets: child,
+        x: 1300,
+        yoyo: true,
+        repeat: -1,
+        ease: 'Sine.easeInOut',
+        duration: 1500,
+        delay: 100 * ci
+      });
 
-    // });
+    });
     // Menu title text with basic style
     const title = this.add
       .text(600, 100, "Menu", { fontSize: "32px", fill: "#0f0" })
