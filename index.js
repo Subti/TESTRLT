f91563a5ed722c64ae346c2c624ae58e9c59862a
Kennedy Gosnell <kennicodes@outlook.com>
2023-11-30 00:46:02 -0800

    change CSS styling plus added light/dark mode button

---
+++ index.js
@@ -0,0 +1,23 @@
+// Non-game related functions
+
+const modeToggleButton = document.getElementById('lightDarkMode');
+const modeIcon = document.getElementById('modeIcon');
+const modeLabel = document.getElementById('modeLabel');
+
+let isLightMode = true; // Initially set to light mode
+
+modeToggleButton.addEventListener('click', () => {
+  // Toggle the CSS classes on all elements with 'light-mode' or 'dark-mode' classes
+  const elementsWithModeClass = document.querySelectorAll('.light-mode, .dark-mode');
+  elementsWithModeClass.forEach((element) => {
+    element.classList.toggle('light-mode', !isLightMode);
+    element.classList.toggle('dark-mode', isLightMode);
+  });
+
+  // Toggle the mode variable
+  isLightMode = !isLightMode;
+
+  // Update the SVG icon source and button label based on the mode
+  modeIcon.src = isLightMode ? '/assets/light_mode.svg' : '/assets/dark_mode.svg';
+  modeLabel.textContent = isLightMode ? 'Light Mode' : 'Dark Mode';
+});
